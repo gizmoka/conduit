e13920d1f7594dd83a39ae57dcbee9199843c030
Gizella <gizus.grundahibaly@gmail.com>
2021-08-11 19:54:53 +0200

    finalised files

--- test-conduit/test_3_registration.py
+++ test-conduit/test_3_registration.py
@@ -5,9 +5,10 @@ from selenium import webdriver
 from datetime import datetime, timezone
 from data import accepting_cookies
 
+
 def test_registration():
     browser_options = Options()
-    browser_options.headless = False
+    browser_options.headless = True
     browser = webdriver.Chrome(ChromeDriverManager().install(), options=browser_options)
     URL = "http://conduitapp.progmasters.hu:1667/#/"
     browser.get(URL)
